736762d3dffe94bd644f8e6069bc81f6dcf0652c
Heger Arias Santos <hegel.0218@gmail.com>
2022-02-08 13:07:31 -0400

    Reset to proyect

--- app/routes/admin/new.tsx
+++
@@ -1,3 +0,0 @@
-export default function NewPost() {
-  return <h2>New Post</h2>;
-}
--- app/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'remix';
-export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <Link to="post">Ir a post</Link>
-    </div>
-  );
-}
